71a9a8c41447c47703783009460879ad09cf59ef
Bakry Rihawi <bakry29bbb@gmail.com>
2024-03-06 15:44:24 +0300

    Editing all messageing.

--- H3BR7/H3BR7.c
+++ H3BR7/H3BR7.c
@@ -455,61 +455,61 @@ Module_Status Module_MessagingTask(uint16_t code,uint8_t port,uint8_t src,uint8_
     IndicatorLED indicator;
 
 	switch(code){
-//	  case CODE_H3BR7_SevenDisplayNumber:
-//	  Number=((int32_t )cMessage[port - 1][shift] ) + ((int32_t )cMessage[port - 1][1 + shift] << 8) + ((int32_t )cMessage[port - 1][2 + shift] << 16) + ((int32_t )cMessage[port - 1][3 + shift] << 24);
-//	  StartSevSeg=(uint8_t)cMessage[port - 1][4+shift];
-//	  SevenDisplayNumber(Number, StartSevSeg);
-//	  break;
-//
-//	  case CODE_H3BR7_SevenDisplayNumberF:
-//		  Number_int=((uint32_t) cMessage[port - 1][shift] + (uint32_t) (cMessage[port - 1][1+shift] <<8) + (uint32_t) (cMessage[port - 1][2+shift]<<16) + (uint32_t) (cMessage[port - 1][3+shift] <<24));
-//		  NumberF = *((float*)&Number_int);
-//		  Res=(uint8_t)cMessage[port - 1][4+shift];
-//		  StartSevSeg=(uint8_t)cMessage[port - 1][5+shift];
-//		  SevenDisplayNumberF(NumberF, Res, StartSevSeg);
-//		  break;
-//
-//	  case CODE_H3BR7_SevenDisplayQuantities:
-//		  Number_int=((uint32_t) cMessage[port - 1][shift] + (uint32_t) (cMessage[port - 1][1+shift] <<8) + (uint32_t) (cMessage[port - 1][2+shift]<<16) + (uint32_t) (cMessage[port - 1][3+shift] <<24));
-//		  NumberF = *((float*)&Number_int);
-//		  Res=(uint8_t)cMessage[port - 1][4+shift];
-//		  Unit=(uint8_t)cMessage[port - 1][5+shift];
-//    	  StartSevSeg=(uint8_t)cMessage[port - 1][6+shift];
-//    	  SevenDisplayQuantities(NumberF, Res, Unit, StartSevSeg);
-//		  break;
-//
-//	  case CODE_H3BR7_SevenDisplayLetter:
-//		  StartSevSeg=(uint8_t)cMessage[port - 1][1+shift];
-//		  SevenDisplayLetter((char)cMessage[port-1][shift], StartSevSeg);
-//		  break;
-//
-//	  case CODE_H3BR7_SevenDisplaySentence:
-//		  length=(uint8_t)cMessage[port - 1][shift];
-//		  StartSevSeg=(uint8_t)cMessage[port - 1][1+shift];
-//		  SevenDisplaySentence((char *)&cMessage[port-1][2 + shift], length, StartSevSeg);
-//		  break;
-//
-//	  case CODE_H3BR7_SevenDisplayMovingSentence:
-//		  length=(uint8_t)cMessage[port - 1][shift];
-//		  SevenDisplayMovingSentence((char *)&cMessage[port-1][1 + shift], length);
-//		  break;
-//
-//	  case CODE_H3BR7_SevenDisplayOff:
-//		  SevenDisplayOff();
-//		  break;
-//
-//	  case CODE_H3BR7_SetIndicator:
-//		  indicator=(uint8_t)cMessage[port - 1][shift];
-//		  SetIndicator(indicator);
-//		  break;
-//
-//	  case CODE_H3BR7_ClearIndicator:
-//		  indicator=(uint8_t)cMessage[port - 1][shift];
-//		  ClearIndicator(indicator);
-//		  break;
-//	  default:
-//			result =H3BR7_ERR_UnknownMessage;
-//			break;
+	  case CODE_H3BRx_SevenDisplayNumber:
+	  Number=((int32_t )cMessage[port - 1][shift] ) + ((int32_t )cMessage[port - 1][1 + shift] << 8) + ((int32_t )cMessage[port - 1][2 + shift] << 16) + ((int32_t )cMessage[port - 1][3 + shift] << 24);
+	  StartSevSeg=(uint8_t)cMessage[port - 1][4+shift];
+	  SevenDisplayNumber(Number, StartSevSeg);
+	  break;
+
+	  case CODE_H3BRx_SevenDisplayNumberF:
+		  Number_int=((uint32_t) cMessage[port - 1][shift] + (uint32_t) (cMessage[port - 1][1+shift] <<8) + (uint32_t) (cMessage[port - 1][2+shift]<<16) + (uint32_t) (cMessage[port - 1][3+shift] <<24));
+		  NumberF = *((float*)&Number_int);
+		  Res=(uint8_t)cMessage[port - 1][4+shift];
+		  StartSevSeg=(uint8_t)cMessage[port - 1][5+shift];
+		  SevenDisplayNumberF(NumberF, Res, StartSevSeg);
+		  break;
+
+	  case CODE_H3BRx_SevenDisplayQuantities:
+		  Number_int=((uint32_t) cMessage[port - 1][shift] + (uint32_t) (cMessage[port - 1][1+shift] <<8) + (uint32_t) (cMessage[port - 1][2+shift]<<16) + (uint32_t) (cMessage[port - 1][3+shift] <<24));
+		  NumberF = *((float*)&Number_int);
+		  Res=(uint8_t)cMessage[port - 1][4+shift];
+		  Unit=(uint8_t)cMessage[port - 1][5+shift];
+    	  StartSevSeg=(uint8_t)cMessage[port - 1][6+shift];
+    	  SevenDisplayQuantities(NumberF, Res, Unit, StartSevSeg);
+		  break;
+
+	  case CODE_H3BRx_SevenDisplayLetter:
+		  StartSevSeg=(uint8_t)cMessage[port - 1][1+shift];
+		  SevenDisplayLetter((char)cMessage[port-1][shift], StartSevSeg);
+		  break;
+
+	  case CODE_H3BRx_SevenDisplaySentence:
+		  length=(uint8_t)cMessage[port - 1][shift];
+		  StartSevSeg=(uint8_t)cMessage[port - 1][1+shift];
+		  SevenDisplaySentence((char *)&cMessage[port-1][2 + shift], length, StartSevSeg);
+		  break;
+
+	  case CODE_H3BRx_SevenDisplayMovingSentence:
+		  length=(uint8_t)cMessage[port - 1][shift];
+		  SevenDisplayMovingSentence((char *)&cMessage[port-1][1 + shift], length);
+		  break;
+
+	  case CODE_H3BRx_SevenDisplayOff:
+		  SevenDisplayOff();
+		  break;
+
+	  case CODE_H3BRx_SetIndicator:
+		  indicator=(uint8_t)cMessage[port - 1][shift];
+		  SetIndicator(indicator);
+		  break;
+
+	  case CODE_H3BRx_ClearIndicator:
+		  indicator=(uint8_t)cMessage[port - 1][shift];
+		  ClearIndicator(indicator);
+		  break;
+	  default:
+			result =H3BR7_ERR_UnknownMessage;
+			break;
 	}
 
 
