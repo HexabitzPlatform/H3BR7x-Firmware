8d12009bce30e410f1d540d477a2f97fdc9c0636
Mahmood_Reda <mr4141705@gmail.com>
2024-04-30 18:37:50 +0300

    Editing main.c and topology.h files

--- User/topology.h
+++ User/topology.h
@@ -1,5 +1,5 @@
 /*
- BitzOS (BOS) V0.3.1 - Copyright (C) 2017-2024 Hexabitz
+ BitzOS (BOS) V0.2.9 - Copyright (C) 2017-2023 Hexabitz
  All rights reserved
 
  File Name     : topology.h
@@ -25,13 +25,13 @@
 
 // Topology
 static uint16_t array[__N ][7] ={
-	{_H3BR7, _mod2 | P1, 0, 0, 0, 0, 0}, 				     				 // Module 1
-	{_H01R0, _mod1 | P1, 0, 0, 0, 0, 0},					    			 // Module 2
+	{_H26R0, _mod2 | P1, 0, 0, 0, 0,0}, 								 // Module 1
+	{_H01R0, _mod1 | P1, 0,0, 0, 0, 0},					    			 // Module 2
 };
 
 // Configurations for duplex serial ports
 #if ( _module == 1 )
-	#define	H3BR7	        1
+	#define	 H26R0	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
