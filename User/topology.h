de12ba2ee1c158e5612b0480997a7f0efb93e763
Mahmood_Reda <mr4141705@gmail.com>
2025-02-10 13:22:03 +0300

    Increased SystemClock from 48MHz to 64MHz. Changed PLL settings: replaced PREDIV/PLLMUL with PLLM/PLLN/PLLP .

--- User/topology.h
+++ User/topology.h
@@ -25,13 +25,13 @@
 
 // Topology
 static uint16_t array[__N ][7] ={
-	{_H26R0, _mod2 | P1, 0, 0, 0, 0,0}, 								 // Module 1
+	{_H3BR7, _mod2 | P1, 0, 0, 0, 0,0}, 								 // Module 1
 	{_H01R0, _mod1 | P1, 0,0, 0, 0, 0},					    			 // Module 2
 };
 
 // Configurations for duplex serial ports
 #if ( _module == 1 )
-	#define	 H26R0	1
+	#define	 H3BR7	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
