93460e619254cd4db2ddbce88509327ab6bff8d1
ADEL\adool <adoolfaki@gmail.com>
2025-04-14 17:33:46 +0300

    Refactort stage 1

--- H3BR7/H3BR7_gpio.c
+++ H3BR7/H3BR7_gpio.c
@@ -1,5 +1,5 @@
 /*
- BitzOS (BOS) V0.3.6 - Copyright (C) 2017-2024 Hexabitz
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
  All rights reserved
 
  File Name     : H3BR7_gpio.c
@@ -7,30 +7,31 @@
 
  */
 
-/* Includes ------------------------------------------------------------------*/
+/* Includes ****************************************************************/
 #include "BOS.h"
 
-/*  */
+/* Local Variables *********************************************************/
 BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t *RX_Port,uint16_t *RX_Pin);
+uint8_t IsFactoryReset(void);
 
-/*----------------------------------------------------------------------------*/
-/* Configure GPIO                                                             */
-/*----------------------------------------------------------------------------*/
+/***************************************************************************/
+/* Configure GPIO **********************************************************/
+/***************************************************************************/
 
-/** Pinout Configuration
- */
+/* Pinout Configuration */
 void GPIO_Init(void){
 	/* GPIO Ports Clock Enable */
 	__GPIOC_CLK_ENABLE();
 	__GPIOA_CLK_ENABLE();
 	__GPIOD_CLK_ENABLE();
 	__GPIOB_CLK_ENABLE();
-	__GPIOF_CLK_ENABLE();		// for HSE and Boot0
+	__GPIOF_CLK_ENABLE();		/* for HSE and Boot0 */
 	
 	IND_LED_Init();
 }
 
-//-- Configure indicator LED
+/***************************************************************************/
+/* Configure indicator LED */
 void IND_LED_Init(void){
 	GPIO_InitTypeDef GPIO_InitStruct;
 	
@@ -40,68 +41,55 @@ void IND_LED_Init(void){
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
 	HAL_GPIO_Init(_IND_LED_PORT,&GPIO_InitStruct);
 }
-//seven seg GPIO Init:
-void seven_seg_gpio_init(void)
-{
 
-	GPIO_InitTypeDef GPIO_InitStruct ={0};
+/***************************************************************************/
+/* Seven Segment GPIO Init */
+void SevenSegGPIOInit(void){
 
-	/* GPIO Ports Clock Enable */
-	__HAL_RCC_GPIOF_CLK_ENABLE();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	__HAL_RCC_GPIOB_CLK_ENABLE();
+	  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOA,Seven_seg_a_Pin | Seven_seg_f_Pin | Seven_seg_b_Pin | Seven_seg_c_Pin | Seven_seg_g_Pin,GPIO_PIN_RESET);
+	  /* GPIO Ports Clock Enable */
+	  __HAL_RCC_GPIOF_CLK_ENABLE();
+	  __HAL_RCC_GPIOA_CLK_ENABLE();
+	  __HAL_RCC_GPIOB_CLK_ENABLE();
 
-	HAL_GPIO_WritePin(GPIOA,Seven_seg_Enable_4_Pin | Seven_seg_Enable_2_Pin | Seven_seg_Enable_6_Pin,GPIO_PIN_SET);
+	  /*Configure GPIO pin Output Level */
+	  HAL_GPIO_WritePin(GPIOA, SEVEN_SEG_A_PIN |SEVEN_SEG_F_PIN|SEVEN_SEG_B_PIN|SEVEN_SEG_C_PIN
+	                          |SEVEN_SEG_G_PIN, GPIO_PIN_RESET);
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOB,Seven_seg_DP_Pin | Seven_seg_d_Pin | Seven_seg_e_Pin,GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOA,SEVEN_SEG_ENABLE_4_PIN|SEVEN_SEG_ENABLE_2_PIN|SEVEN_SEG_ENABLE_6_PIN, GPIO_PIN_SET);
 
-	HAL_GPIO_WritePin(GPIOB,Seven_seg_Enable_1_Pin | Seven_seg_Enable_3_Pin | Seven_seg_Enable_5_Pin,GPIO_PIN_SET);
+	  /*Configure GPIO pin Output Level */
+	  HAL_GPIO_WritePin(GPIOB, SEVEN_SEG_DP_PIN|SEVEN_SEG_D_PIN|SEVEN_SEG_E_PIN, GPIO_PIN_RESET);
 
-	/*Configure GPIO pins : A_Pin F_Pin B_Pin
-	 G_Pin CA1_Pin CA2_Pin  */
-	GPIO_InitStruct.Pin = Seven_seg_a_Pin | Seven_seg_f_Pin | Seven_seg_b_Pin | Seven_seg_g_Pin | Seven_seg_Enable_1_Pin | Seven_seg_Enable_2_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOA,&GPIO_InitStruct);
+	  HAL_GPIO_WritePin(GPIOB,SEVEN_SEG_ENABLE_1_PIN|SEVEN_SEG_ENABLE_3_PIN|SEVEN_SEG_ENABLE_5_PIN| COMMON_LED_PIN,GPIO_PIN_SET);
 
-	/*Configure GPIO pins : DP_Pin C_Pin D_Pin E_Pin CA3_Pin
-	 CA4_Pin  LEDs(1,2,3,4)  */
-	GPIO_InitStruct.Pin = Seven_seg_DP_Pin | Seven_seg_d_Pin | Seven_seg_e_Pin | Seven_seg_Enable_1_Pin | Seven_seg_c_Pin | Seven_seg_Enable_3_Pin | Seven_seg_Enable_4_Pin | Led_Indicator1_Pin | Led_Indicator2_Pin | Led_Indicator3_Pin | Led_Indicator4_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOB,&GPIO_InitStruct);
+	  /*Configure GPIO pins : A_Pin F_Pin B_Pin C_Pin
+	                           G_Pin CA3_Pin CA4_Pin CA1_Pin */
+	  GPIO_InitStruct.Pin = SEVEN_SEG_A_PIN|SEVEN_SEG_F_PIN|SEVEN_SEG_B_PIN|SEVEN_SEG_C_PIN
+	                          |SEVEN_SEG_G_PIN|SEVEN_SEG_ENABLE_4_PIN|SEVEN_SEG_ENABLE_2_PIN|SEVEN_SEG_ENABLE_6_PIN;
+	  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	  GPIO_InitStruct.Pull = GPIO_NOPULL;
+	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-	/*Configure GPIO pin Output Level */
-	HAL_GPIO_WritePin(GPIOB,Led_Indicator1_Pin | Led_Indicator2_Pin | Led_Indicator3_Pin | Led_Indicator4_Pin,GPIO_PIN_SET);
-
-	/*Configure GPIO pins : CA5_Pin CA6_Pin  */
-
-	GPIO_InitStruct.Pin =Seven_seg_Enable_5_Pin | Seven_seg_Enable_6_Pin;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-	HAL_GPIO_Init(GPIOD,&GPIO_InitStruct);
-
+	  /*Configure GPIO pins : DP_Pin D_Pin E_Pin CA6_Pin
+	                           CA5_Pin  CA2_Pin */
+	  GPIO_InitStruct.Pin = SEVEN_SEG_DP_PIN|SEVEN_SEG_D_PIN|SEVEN_SEG_E_PIN|SEVEN_SEG_ENABLE_1_PIN
+	                          |SEVEN_SEG_ENABLE_3_PIN|SEVEN_SEG_ENABLE_5_PIN|COMMON_LED_PIN;
+	  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	  GPIO_InitStruct.Pull = GPIO_NOPULL;
+	  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
 
-/*-----------------------------------------------------------*/
-/*-----------------------------------------------------------*/
-
-/* --- Check for factory reset condition: 
- - P1 TXD is connected to last port RXD    
- */
+/***************************************************************************/
+/* Check for factory reset condition:
+ - P1 TXD is connected to last port RXD  */
 uint8_t IsFactoryReset(void){
 	GPIO_InitTypeDef GPIO_InitStruct;
 	uint32_t P1_TX_Port, P1_RX_Port, P_last_TX_Port, P_last_RX_Port;
 	uint16_t P1_TX_Pin, P1_RX_Pin, P_last_TX_Pin, P_last_RX_Pin;
-	
-	/* -- Setup GPIOs -- */
 
 	/* Enable all GPIO Ports Clocks */
 	__GPIOA_CLK_ENABLE();
@@ -143,10 +131,8 @@ uint8_t IsFactoryReset(void){
 	return 0;
 }
 
-/*-----------------------------------------------------------*/
-
-/* --- Get GPIO pins and ports of this array port
- */
+/***************************************************************************/
+/* Get GPIO pins and ports of this array port */
 BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t *RX_Port,uint16_t *RX_Pin){
 	BOS_Status result =BOS_OK;
 	
@@ -154,14 +140,14 @@ BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t
 	UART_HandleTypeDef *huart =GetUart(port);
 	
 	if(huart == &huart1){
-#ifdef _Usart1		
+#ifdef _USART1
 		*TX_Port =(uint32_t ) USART1_TX_PORT;
 		*TX_Pin = USART1_TX_PIN;
 		*RX_Port =(uint32_t ) USART1_RX_PORT;
 		*RX_Pin = USART1_RX_PIN;
 #endif
 	}
-#ifdef _Usart2	
+#ifdef _USART2
 	else if(huart == &huart2){
 		*TX_Port =(uint32_t ) USART2_TX_PORT;
 		*TX_Pin = USART2_TX_PIN;
@@ -169,7 +155,7 @@ BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t
 		*RX_Pin = USART2_RX_PIN;
 	}
 #endif
-#ifdef _Usart3	
+#ifdef _USART3
 	else if(huart == &huart3){
 		*TX_Port =(uint32_t ) USART3_TX_PORT;
 		*TX_Pin = USART3_TX_PIN;
@@ -177,7 +163,7 @@ BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t
 		*RX_Pin = USART3_RX_PIN;
 	}
 #endif
-#ifdef _Usart4
+#ifdef _USART4
 	else if(huart == &huart4){
 		*TX_Port =(uint32_t ) USART4_TX_PORT;
 		*TX_Pin = USART4_TX_PIN;
@@ -185,7 +171,7 @@ BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t
 		*RX_Pin = USART4_RX_PIN;
 	}
 #endif
-#ifdef _Usart5	
+#ifdef _USART5
 	else if(huart == &huart5){
 		*TX_Port =(uint32_t ) USART5_TX_PORT;
 		*TX_Pin = USART5_TX_PIN;
@@ -193,7 +179,7 @@ BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t
 		*RX_Pin = USART5_RX_PIN;
 	}
 #endif
-#ifdef _Usart6	
+#ifdef _USART6
 	else if(huart == &huart6){
 		*TX_Port =(uint32_t ) USART6_TX_PORT;
 		*TX_Pin = USART6_TX_PIN;
@@ -201,7 +187,7 @@ BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t
 		*RX_Pin = USART6_RX_PIN;
 	}
 #endif
-#ifdef _Usart7
+#ifdef _USART7
 	else if (huart == &huart7) 
 	{		
 		*TX_Port = (uint32_t)USART7_TX_PORT;
@@ -210,7 +196,7 @@ BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t
 		*RX_Pin = USART7_RX_PIN;
 	} 
 #endif
-#ifdef _Usart8	
+#ifdef _USART8
 	else if (huart == &huart8) 
 	{	
 		*TX_Port = (uint32_t)USART8_TX_PORT;
@@ -225,4 +211,5 @@ BOS_Status GetPortGPIOs(uint8_t port,uint32_t *TX_Port,uint16_t *TX_Pin,uint32_t
 	return result;
 }
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+/***************************************************************************/
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
